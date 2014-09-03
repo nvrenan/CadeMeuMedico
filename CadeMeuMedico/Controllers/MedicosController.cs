de91315a23a86b386e7166545c6a87eeee0b9afe
Renan <nvrenan@gmailcom>
2014-09-03 20:06:27 -0300

    Versionando o projeto

---
+++ CadeMeuMedico/Controllers/MedicosController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Linq;
+using CadeMeuMedico.Models;
+
+namespace CadeMeuMedico.Controllers
+{
+    public class MedicosController : Controller
+    {
+        //
+        // GET: /Medicos/
+        private EntidadesCadeMeuMedicoBD db = new EntidadesCadeMeuMedicoBD();
+        public ActionResult Index()
+        {
+            var medicos = db.Medicos.Include(m => m.Cidades).Include(m => m.Especialidades).ToList();
+            return View(medicos);
+        }
+
+    }
+}
