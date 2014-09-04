ab9bd886e4bcddb8174ee1c974c202094ff72a8f
Renan <nvrenan@gmailcom>
2014-09-04 20:59:35 -0300

    Adicionando as views de medico

--- CadeMeuMedico/App_Start/RouteConfig.cs
+++ CadeMeuMedico/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CadeMeuMedico
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "TesteLayout", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- CadeMeuMedico/Controllers/MedicosController.cs
+++ CadeMeuMedico/Controllers/MedicosController.cs
@@ -11,8 +11,17 @@ namespace CadeMeuMedico.Controllers
         private EntidadesCadeMeuMedicoBD db = new EntidadesCadeMeuMedicoBD();
         public ActionResult Index()
         {
-            var medicos = db.Medicos.Include(m => m.Cidades).Include(m => m.Especialidades).ToList();
+            var medicos = db.Medicos.Include("Cidades").Include("Especialidades").ToList();
             return View(medicos);
+        }
+
+        public ActionResult Adicionar()
+        {
+            ViewBag.IDCidade = new SelectList(db.Cidades, "IDCidade", "Nome");
+            ViewBag.IDEspecialidade = new SelectList(db.Especialidades,
+                "IDEspecialidade",
+                "Nome");
+            return View();
         }
 
     }
