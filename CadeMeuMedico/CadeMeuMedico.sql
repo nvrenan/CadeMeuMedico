9d146b01537dcf2eb67149b2f56c1f0c8821a85a
Renan <nvrenan@gmailcom>
2014-09-13 18:29:34 -0300

    Adicionando o banner

--- CadeMeuMedico/CadeMeuMedico.sql
+++ CadeMeuMedico/CadeMeuMedico.sql
@@ -74,3 +74,20 @@ INSERT INTO Especialidade (Nome) VALUES ('Neurologista')
 GO
  
 Insert Into Usuario (Nome, Login, Senha, Email) Values ('Administrador', 'admin', '40BD001563085FC35165329EA1FF5C5ECBDBBEEF','admin@cdmm.com')
+
+CREATE TABLE BannersPublicitarios
+(
+IDBanner BIGINT IDENTITY NOT NULL,
+TituloCampanha VARCHAR(60) NOT NULL,
+BannerCampanha VARCHAR(200) NOT NULL,
+LinkBanner VARCHAR(200) NULL,
+PRIMARY KEY(IDBanner)
+);
+
+INSERT INTO BannersPublicitarios
+(TituloCampanha, BannerCampanha, LinkBanner) VALUES
+('Campanha Conio', 'logo-conio-cademeumedico.png','http://conio.com.br')
+INSERT INTO BannersPublicitarios
+(TituloCampanha, BannerCampanha, LinkBanner) VALUES
+('Campanha Casa do Código', 'banner-cdc-cademeumedico.png',
+'http://casadocodigo.com.br')
